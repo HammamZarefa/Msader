fea218eac93e646a519f036478a9365b4176d901
mohammed-assi <mohammed@futurecode-co.com>
2023-07-16 04:17:12 +0300

    add details to view

--- resources/views/admin/pages/log/show.blade.php
+++ resources/views/admin/pages/log/show.blade.php
@@ -48,6 +48,48 @@
                 </div>
             </div>
 
+            <div class="col-md-4 col-xl-3">
+                <div class="form-group">
+                    <select name="header" id="header" class="form-control statusfield">
+                        <option value="-1"
+                                @if(@request()->header == '-1') selected @endif>@lang('All header')</option>
+                        @foreach($logs as $log)
+                            <option value="{{$log->header}}"
+                                    @if(@request()->header == $log->header) selected @endif>@lang($log->header)</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+
+
+           
+
+
+            <div class="col-md-4 col-xl-3">
+                <div class="form-group">
+                    <select name="body" id="body" class="form-control statusfield">
+                        <option value="-1"
+                                @if(@request()->body == '-1') selected @endif>@lang('All body')</option>
+                        @foreach($logs as $log)
+                            <option value="{{$log->body}}"
+                                    @if(@request()->body == $log->body) selected @endif>@lang($log->body)</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+
+            <div class="col-md-4 col-xl-3">
+                <div class="form-group">
+                    <select name="disclosure" id="disclosure" class="form-control statusfield">
+                        <option value="-1"
+                                @if(@request()->disclosure == '-1') selected @endif>@lang('All disclosure')</option>
+                        @foreach($logs as $log)
+                            <option value="{{$log->disclosure}}"
+                                    @if(@request()->disclosure == $log->disclosure) selected @endif>@lang($log->disclosure)</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
 
             <div class="col-md-4 col-xl-3">
                 <div class="form-group">
@@ -72,7 +114,9 @@
                             <th scope="col">order_id</th>
                             <th scope="col">url</th>
                             <th scope="col">method</th>
-                           
+                            <th scope="col">header</th>
+                            <th scope="col">body</th>
+                            <th scope="col">disclosure</th>
                         </tr>
                         </thead>
                         <tbody>
@@ -82,6 +126,9 @@
                                 <td>{{$logs_search->order_id}}</td>
                                 <td>{{$logs_search->url}}</td>
                                 <td>{{$logs_search->method}}</td>
+                                <td>{{$logs_search->header}}</td>
+                                <td>{{$logs_search->body}}</td>
+                                <td>{{$logs_search->disclosure}}</td>
                                
                                 {{-- <td data-label="@lang('Title')">@lang($notice->title) </td>
                                 <td data-label="@lang('Highlight text')"><span class="badge badge-info">@lang($notice->highlight_text)</span> </td>
