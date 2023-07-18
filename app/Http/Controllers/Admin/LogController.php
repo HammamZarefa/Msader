e0539d7f0af635fa0ce49a57f0298a6a3059e1c6
mohammed-assi <mohammed@futurecode-co.com>
2023-07-18 19:37:04 +0300

    fix provider log blade

--- app/Http/Controllers/Admin/LogController.php
+++ app/Http/Controllers/Admin/LogController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\ApiProvider;
 use App\Models\ProviderLog;
 use Illuminate\Http\Request;
 
@@ -16,40 +17,17 @@ class LogController extends Controller
     public function search(Request $request){
         $search = $request->all();
         $logs_searchs = ProviderLog::
-            when(isset($search['order']), function ($query) use ($search) {
-                if ($search['order'] != -1) {
-                   return $query->where('order_id', 'LIKE', "%{$search['order']}%");
-                }
-             })
-             -> when(isset($search['url']), function ($query) use ($search) {
+            when(isset($search['url']), function ($query) use ($search) {
                 if ($search['url'] != -1) {
                    return $query->where('url', 'LIKE', "%{$search['url']}%");
                 }
              })
-             -> when(isset($search['method']), function ($query) use ($search) {
-                if ($search['method'] != -1) {
-                   return $query->where('method', 'LIKE', "%{$search['method']}%");
-                }
-             })
-             -> when(isset($search['header']), function ($query) use ($search) {
-               if ($search['header'] != -1) {
-                  return $query->where('header', 'LIKE', "%{$search['header']}%");
-               }
-            })
-            -> when(isset($search['body']), function ($query) use ($search) {
-               if ($search['body'] != -1) {
-                  return $query->where('body', 'LIKE', "%{$search['body']}%");
-               }
-            })
-            -> when(isset($search['disclosure']), function ($query) use ($search) {
-               if ($search['disclosure'] != -1) {
-                  return $query->where('disclosure', 'LIKE', "%{$search['disclosure']}%");
-               }
-            })
-            ->get();
+            ->orderBy('created_at', 'desc')->get();
 
             $logs = ProviderLog::all();
-            return view('admin.pages.log.show', compact('logs','logs_searchs'));
+            $provider_api = ApiProvider::all();
+            // dd($logs[55]['disclosure']['order_id']);
+            return view('admin.pages.log.show', compact('provider_api','logs_searchs'));
 
     }
 
--- app/Models/ProviderLog.php
+++ app/Models/ProviderLog.php
@@ -6,11 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProviderLog extends Model
 {
-    // protected $casts = [
-    //     'header' => 'json',
-    //     'body' => 'json',
-    //     'disclosure' => 'json',
-    // ];
+    protected $casts = [
+        'header' => 'json',
+        'body' => 'json',
+        'disclosure' => 'json',
+    ];
 
 
     protected $fillable = ['order_id', 'url', 'method', 'header', 'body', 'disclosure'];
